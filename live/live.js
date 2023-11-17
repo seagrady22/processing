1030e7389af188db23459849a524aafbf67fd3a6
offeringAdmin <offeringllc@gmail.com>
2023-11-17 11:10:06 -0800

    Adding peak detect and mapping functions..

--- live/live.js
+++ live/live.js
@@ -5,6 +5,13 @@ let spectra = [];
 let mic; 
 let index;
 let FRAME_RATE = 15;
+let FFP = 40;
+		
+//the band
+let E;
+let F;
+let W;
+let A;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -15,6 +22,16 @@ function setup() {
 
 	// Getting FFT from p5js
 	fft = new p5.FFT(0.5, 64);
+	
+	E = new p5.PeakDetect(20,288,0.35, FFP);
+	F = new p5.PeakDetect(144,720, 0.35, FFP);
+	W = new p5.PeakDetect(570, 1440, 0.35, FFP);
+	A = new p5.PeakDetect(1440, 11520, 0.35, FFP);
+	
+  E.onPeak(Earth);
+	F.onPeak(Fire);
+	W.onPeak(Water);
+	A.onPeak(Air);
 	
   mic.start();
 	mic.connect(fft);
@@ -29,4 +46,20 @@ function setup() {
 
 function draw() {
 	circle(mouseX, mouseY, 20);
+}
+
+function Earth() {
+
+}
+
+function Fire() {
+
+}
+
+function Water() {
+
+}
+
+function Air() {
+
 }
