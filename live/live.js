2a5f0e8e48a87b61f3cb83a36c802e7d69e825a5
offeringAdmin <offeringllc@gmail.com>
2023-11-17 11:50:12 -0800

    Drawing random shapes, printing the height of the spectrum

--- live/live.js
+++ live/live.js
@@ -6,7 +6,7 @@ let mic;
 let index;
 let FRAME_RATE = 15;
 let FFP = 40;
-		
+let SPEC_MAX_LEN = 30;
 //the band
 let E;
 let F;
@@ -26,7 +26,7 @@ function setup() {
 	E = new p5.PeakDetect(20,288,0.35, FFP);
 	F = new p5.PeakDetect(144,720, 0.35, FFP);
 	W = new p5.PeakDetect(570, 1440, 0.35, FFP);
-	A = new p5.PeakDetect(1440, 11520, 0.35, FFP);
+	A = new p5.PeakDetect(1296, 11520, 0.35, FFP);
 	
   E.onPeak(Earth);
 	F.onPeak(Fire);
@@ -45,7 +45,55 @@ function setup() {
 }
 
 function draw() {
-	circle(mouseX, mouseY, 20);
+	let spectrum = fft.analyze();
+	
+	//add the new values from the FFT
+	spectra.push(new Spectrum(spectrum));
+	
+	//cap at max len insert new at begin & delete 1
+	if (spectra.length > SPEC_MAX_LEN) {
+		let deleteCount = 1; let startIndex = 0;
+		spectra.splice(startIndex, deleteCount);
+	}
+	
+		//strokeWeight(1);
+	//noStroke();
+	for (j = 0; j < spectra.length; j++) {
+		let spec = spectra[j].getSpectrum();
+		
+		for (i = 0; i < 128; i += 1) {
+			
+			adjust = (i + 1) * (i * 1) / 90; 
+			
+			let hh = 3;
+			let h = map(spec[i] * adjust, 0, 255, 0, hh);
+			
+			print(h);
+			
+			if (h > 10)
+				{
+					drawRandomShape();
+					
+				}
+			/*
+			stroke(100, 0, 0);
+			fill((i)*2, 100, 100);
+			//ambientMaterial();
+			// use specular material with high shininess
+  		//specularMaterial((i+2)*10,  0, 0);
+ 			//shininess(50);
+			push();
+			//noStroke();
+			translate(i, j, h/2);
+			//rotateX(PI/2);
+			//rotateZ(PI/2);
+			//cylinder(0.5, h);
+			box(0.85, 0.85, h)
+			pop();
+			*/
+		}
+	}
+	
 }
 
 function Earth() {
@@ -62,4 +110,35 @@ function Water() {
 
 function Air() {
 
+}
+
+// Define class for Spectrum
+class Spectrum {
+ constructor(spectrum){
+   this.spectrum = spectrum;
+ }
+ getSpectrum() {
+   for(let i=0; i<this.spectrum.length; i++){
+     this.spectrum[i]*=0.95;
+   }
+   return this.spectrum;
+ }
+}
+
+function drawRandomShape(choice) {
+  x = random(width);
+  y = random(height);
+  w = random(5, 100);
+  h = random(5, 100);
+  
+  if (choice == "ellipse") {
+    noStroke();
+    fill(random(25), random(5), random(255), 50);
+    ellipse(x, y, w, h);
+  }
+  else {
+    noStroke();
+    fill(random(255), random(255), random(255), 90);
+    rect(x, y, w, h);
+  }
 }
